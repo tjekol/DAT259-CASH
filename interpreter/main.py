f2bde43f65e8146cc2552112283446dcb13dc16f
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-05-03 20:55:32 +0200

    boolean expressions

--- interpreter/main.py
+++ interpreter/main.py
@@ -9,22 +9,7 @@ class InterpreterVisitor(CASHVisitor):
     def __init__(self, symbol_table: SymbolTable):
         self.symbol_table = symbol_table
 
-    def visitPrint(self, ctx: CASHParser.PrintContext):
-        empty_string = ""
-        if ctx.STRING() is not None: 
-            string_token = ctx.STRING()
-            empty_string += str(string_token)[1:-1]
-
-        if ctx.expression() is not None:
-            value = self.visit(ctx.expression())
-            empty_string += str(value)
-        print(str(empty_string))
-
-    def visitCost(self, ctx: CASHParser.CostContext):
-        name = str(ctx.IDENTIFIER())
-        value = self.visit(ctx.expression())
-        self.symbol_table.add_var(name,value)
-
+    # EXPRESSIONS
     def visitNested(self, ctx: CASHParser.NestedContext):
         return self.visit(ctx.expression())
 
@@ -39,6 +24,21 @@ class InterpreterVisitor(CASHVisitor):
     
     def visitSub(self, ctx: CASHParser.SubContext):
         return self.visit(ctx.getChild(0)) - self.visit(ctx.getChild(2))
+    
+    def visitNested_bool(self, ctx: CASHParser.Nested_boolContext):
+        return self.visit(ctx.bool_expr())
+    
+    def visitAnd(self, ctx: CASHParser.AndContext):
+        return self.visit(ctx.getChild(0)) and self.visit(ctx.getChild(2))
+    
+    def visitOr(self, ctx: CASHParser.OrContext):
+        return self.visit(ctx.getChild(0)) or self.visit(ctx.getChild(2))
+    
+    def visitNot(self, ctx: CASHParser.NotContext):
+        return not self.visit(ctx.bool_expr())
+
+    def visitComp(self, ctx: CASHParser.CompContext):
+        return self.visit(ctx.comparison())
     
     def visitComparison(self, ctx: CASHParser.ComparisonContext):
         left = self.visit(ctx.getChild(0))
@@ -55,13 +55,7 @@ class InterpreterVisitor(CASHVisitor):
         elif ctx.COMPARE_GTE() is not None:
             return left >= right
 
-    def visitDiscount(self, ctx: CASHParser.DiscountContext):
-        exp1 = self.visit(ctx.expression())
-        name = str(ctx.IDENTIFIER())
-        exp2 = self.symbol_table.get_var(name)
-        exp2 = exp2 - (exp2 * (float(exp1) / 100))
-        self.symbol_table.add_var(name, exp2)
-
+    # TYPES
     def visitInt(self, ctx: CASHParser.IntContext):
         return int(str(ctx.INT()))
     
@@ -73,6 +67,31 @@ class InterpreterVisitor(CASHVisitor):
         var = self.symbol_table.get_var(name)
         return var
     
+    
+    # STATEMENTS
+    def visitPrint(self, ctx: CASHParser.PrintContext):
+        empty_string = ""
+        if ctx.STRING() is not None: 
+            string_token = ctx.STRING()
+            empty_string += str(string_token)[1:-1]
+
+        if ctx.expression() is not None:
+            value = self.visit(ctx.expression())
+            empty_string += str(value)
+        print(str(empty_string))
+
+    def visitCost(self, ctx: CASHParser.CostContext):
+        name = str(ctx.IDENTIFIER())
+        value = self.visit(ctx.expression())
+        self.symbol_table.add_var(name,value)
+
+    def visitDiscount(self, ctx: CASHParser.DiscountContext):
+        exp1 = self.visit(ctx.expression())
+        name = str(ctx.IDENTIFIER())
+        exp2 = self.symbol_table.get_var(name)
+        exp2 = exp2 - (exp2 * (float(exp1) / 100))
+        self.symbol_table.add_var(name, exp2)
+    
     def visitAsk(self, ctx: CASHParser.AskContext):
         prompt = str(ctx.STRING())[1:-1]
         value = float(input(f"{prompt}: "))
@@ -80,18 +99,20 @@ class InterpreterVisitor(CASHVisitor):
         self.symbol_table.add_var(name, value)
 
     def visitCond_mod(self, ctx: CASHParser.Cond_modContext):
-        for c, stmt in zip(ctx.comparison(), ctx.main_stmt()):
+        for c, stmt in zip(ctx.bool_expr(), ctx.main_stmt()):
             if self.visit(c):
                 self.visit(stmt)
                 return
-        self.visit(ctx.main_stmt(len(ctx.comparison())))
+        if len(ctx.main_stmt()) <= len(ctx.bool_expr()):
+            return
+        self.visit(ctx.main_stmt(len(ctx.bool_expr())))
 
     def visitScan_mod(self, ctx: CASHParser.Scan_modContext):
-        isSatisfied = self.visit(ctx.comparison())
+        isSatisfied = self.visit(ctx.bool_expr())
         while isSatisfied:
             for stmt in ctx.main_stmt():
                 self.visit(stmt)
-            isSatisfied = self.visit(ctx.comparison())
+            isSatisfied = self.visit(ctx.bool_expr())
 
     def visitTask_mod(self, ctx: CASHParser.Task_modContext):
         name = str(ctx.IDENTIFIER(0))
@@ -128,6 +149,7 @@ class InterpreterVisitor(CASHVisitor):
 def main():
     input_stream = FileStream("./example_code/helloWorld.csh", encoding="utf-8")
     # input_stream = FileStream("./example_code/calc.csh", encoding="utf-8")
+    # input_stream = FileStream("./example_code/boolExpr.cash", encoding="utf-8")
     # input_stream = FileStream("./example_code/fibonacci.csh", encoding="utf-8")
     # input_stream = FileStream("./example_code/func.csh", encoding="utf-8")
     # input_stream = FileStream("./example_code/ifThenElse.csh", encoding="utf-8")
